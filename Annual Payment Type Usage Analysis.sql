3a16bd7291388d2e8c2d98e84fda3250d81b2729
Arif Budiman <abudiman357@gmail.com>
2022-11-06 11:09:30 +0700

    V 0.2
    Update Annual Payment Type Usage Analysis

--- Annual Payment Type Usage Analysis.sql
+++ Annual Payment Type Usage Analysis.sql
@@ -1,37 +1,29 @@
 -- Step 1
 -- The amount of use of each type of payment all the time sorted from the most favorite
 
-select 
+SELECT
 	payment_type,
-	count(*) as total_used
-from order_payments op
-	join orders o 
-        on op.order_id = o.order_id
-group by 1
-order by 2 desc;
+	COUNT(order_id) AS payment_type_usage
+FROM order_payments
+GROUP BY 1
+ORDER BY 2 DESC;
 
 -- Step 2
 -- Information on the amount of use of each type of payment for each year
 
-select
-	date_part('year', o.order_purchase_timestamp) AS purchase_year,
-	op.payment_type,
-	count(*) as transactions
-from orders o
-	join order_payments op 
-        on o.order_id = op.order_id
-group by 1,2
-order by 1, 3 desc;
-	
-select
-	date_part('year', o.order_purchase_timestamp) as purchase_year,
-	sum(case when (op.payment_type='credit_card') then 1 else 0 end) as credit_card,
-	sum(case when (op.payment_type='boleto') then 1 else 0 end) as boleto,
-	sum(case when (op.payment_type='voucher') then 1 else 0 end) as voucher,
-	sum(case when (op.payment_type='debit_card') then 1 else 0 end) as debit_card,
-	sum(case when (op.payment_type='not_defined') then 1 else 0 end) as not_defined
-from orders o
-	join order_payments op 
-        on o.order_id = op.order_id
-group by 1
-order by 1;
+SELECT
+	payment_type,
+	SUM(CASE WHEN year = 2016 THEN payment_type_usage ELSE 0 END) AS "year_2016",
+	SUM(CASE WHEN year = 2017 THEN payment_type_usage ELSE 0 END) AS "year_2017",
+	SUM(CASE WHEN year = 2018 THEN payment_type_usage ELSE 0 END) AS "year_2018",
+	SUM(payment_type_usage) AS sum_payment_type_usage
+FROM (SELECT
+	  	DATE_PART('year', order_purchase_timestamp) AS year,
+	 	payment_type,
+	 	COUNT(payment_type) AS payment_type_usage
+	  FROM orders AS o
+	  JOIN order_payments AS op ON op.order_id = o.order_id
+	  GROUP BY 1, 2
+	 ) AS subq
+GROUP BY 1
+ORDER BY 2 DESC;
